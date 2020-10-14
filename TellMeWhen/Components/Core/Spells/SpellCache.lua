5a826cdea8dce88ccce0f10ef2ade74c2f721413
MisterCrab <51509374+MisterCrab@users.noreply.github.com>
2020-10-14 07:20:09 +0300

    TMW - Unlocked spell cache (forgot to do)

--- TellMeWhen/Components/Core/Spells/SpellCache.lua
+++ TellMeWhen/Components/Core/Spells/SpellCache.lua
@@ -366,7 +366,7 @@ TMW:RegisterCallback("TMW_OPTIONS_LOADED", function()
 		local start = debugprofilestop()
 
 		local success = TMW.safecall(SpellCacher)
-		if success and spellsFailed < MAX_FAILED_SPELLS and spellID < 293000 then -- temporary disable
+		if success and spellsFailed < MAX_FAILED_SPELLS then
 			-- Carry on. Keep iterating.
 		else
 			-- We're done, or we errored.
